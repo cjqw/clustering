c2fbf50f85d46005fb9f57f1a9938b2dc9a95a01
cjqw <2462457751@qq.com>
2017-05-13 18:38:32 +0800

    draw 3d graph of the points

--- src/main.py
+++ src/main.py
@@ -1,31 +1,53 @@
 #!env /usr/bin/python3
 
 import numpy as np
-from random import randint
 from math import *
-from kmeans import *
+from kmeans import KMeans
+from util.util import *
 
-def parseItem(item):
-    dims = item.split(',')
-    return [float(dim) for dim in dims]
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def parseLine(line):
+    dims = line.split(',')
+    return mapv(float,dims[:-1]), int(float(dims[3]))
 
 def readData():
     inputFile = 'data/data.csv'
     with open(inputFile,'r') as fin:
-        data = fin.readlines()[1:]
-    return [parseItem(item) for item in data]
+        lines = fin.readlines()[1:]
+    result = mapv(parseLine,lines)
+    return mapv(getItem(0),result), mapv(getItem(1),result)
 
 def distance(x,y):
     return sqrt((x[0]-y[0])**2
                 +(x[1]-y[1])**2
                 +(x[2]-y[2])**2)
 
+def draw(data,label):
+    model = partition(list(zip(data,label)),getItem(1))
+    ax=plt.subplot(111,projection='3d')
+
+    for label in model:
+        position = mapv(getItem(0),model[label])
+        xs = mapv(getItem(0),position)
+        ys = mapv(getItem(1),position)
+        zs = mapv(getItem(2),position)
+        ax.scatter(xs,ys,zs)
+
+    ax.set_zlabel('Z')
+    ax.set_ylabel('Y')
+    ax.set_xlabel('X')
+    plt.show()
+
 if __name__ == "__main__":
-    data = readData()
-    ans = 100
+    data,label = readData()
+    # draw(data,label)
+    ans = {'maximum distance':9999,"labels":[]}
+    maxDistance = getItem('maximum distance')
     for i in range(100):
-        label = KMeans(data,3,distance).solve()
-        maximumDistance = label["maximum distance"]
-        print(maximumDistance)
-        ans = min(ans,maximumDistance)
+        result = KMeans(data,3,distance).solve()
+        if(maxDistance(ans) > maxDistance(result)):
+            ans = result
     print(ans)
+    draw(data,ans['labels'])
